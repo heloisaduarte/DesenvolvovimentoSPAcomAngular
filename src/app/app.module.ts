e79d59652427a27298346cfe4320b767fd9e5453
Heloisa Duarte <duarteh729@gmail.com>
2023-09-19 09:11:41 -0300

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HugoLindoComponent } from './hugo-lindo/hugo-lindo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HugoLindoComponent
   ],
   imports: [
     BrowserModule
